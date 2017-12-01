293234a249576cbc68b8db75d682e75f2a8839b2
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-01 16:30:28 -0600

    Otra linea de codigo

--- array.php
+++ array.php
@@ -10,7 +10,15 @@ echo $semana[4];
 //    Esto es opcional ..
 
 $semana[23] = "Ese dia no exite";
-echo "<br>".$semana[23];
+echo "<br>".$semana[23]."<br>";
+
+//    recorrer
+echo $semana[0]."<br>";
+echo $semana[1]."<br>";
+echo $semana[2]."<br>";
+echo $semana[3]."<br>";
+echo $semana[4]."<br>";
+echo $semana[5]."<br>";
 
 
 
