dc81f4cc23c49432f2d8147581cfca1feaf73c7c
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-01 16:52:27 -0600

    arreglos asociative

---
+++ array_asosiative.php
@@ -0,0 +1,25 @@
+<?php
+
+  $Telefono =  5879422;
+  $edad = 29;
+  $apellido = "Alanis";
+  $pais = "México";
+  $telefono2 = 454545454;
+
+
+   $Jon = array('telefono' => $Telefono, 'edad' => $edad, 'apellido'=>$apellido ,'pais'=>$pais );
+
+   echo "telefono : ".$Jon['telefono']."<br>";
+   echo "telefono 2 : ".$Jon['telefono'] = $telefono2 ."<br>";
+   echo "edad : ".$Jon['edad']."<br>";
+   echo "apellido: ".$Jon['apellido']."<br>";
+    echo "pais : ".$Jon['pais']."<br>";
+
+
+
+
+
+
+
+
+ ?>
