d9152a38462158b16050e66f8d8a1fb26eb1afc0
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-01 17:23:27 -0600

    arreglos multidimensional

---
+++ array_multidimensional.php
@@ -0,0 +1,11 @@
+<?php
+
+$fiends =    array( array('Juan',25),
+             array('carolina',23),
+             array('lupita',25));
+
+echo "Amigos 1:"." ".$fiends[0][0]."  ".$fiends[0][1]."<br>" ;
+echo "Amigos 2:"." ".$fiends[1][0]."  ".$fiends[1][1]."<br>" ;
+echo "Amigos 3:"." ".$fiends[2][0]."  ".$fiends[2][1]."<br>" ;
+
+ ?>
