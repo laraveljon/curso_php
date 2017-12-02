6bb26fb7478e0346a12b91ad506af9415feca4f9
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-02 11:20:27 -0600

    Formulario para pone fechas

---
+++ switch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/ajax.js"></script>
+  </head>
+  <body>
+    <center>
+      <label for="">
+        Introdusca el nombre de la fechas :
+      <input type="text" id="nombre" name="nombre"/>
+      </label>
+      <br /><br />
+      <input type="button" value="Enviar"/ onclick="Enviar();">
+
+      <div id="respuesta">
+
+
+        
+      </div>
+    </center>
+  </body>
+</html>
