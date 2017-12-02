ce106616fa180826643d94a88b45754cb26f77a0
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-02 11:18:34 -0600

    Libreria query

--- js/jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).on("ready", function(){
-	$("#mostrar-modal").on("click", function(){
-		$("#modal").addClass("mostrar-modal");
-	});
-
-	$("#cerrar-modal").on("click", function(){
-		$("#modal").removeClass("mostrar-modal");
-	});
-})
