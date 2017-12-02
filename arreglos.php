74dc16ed2bec58409943f28d637fb963f26a9052
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-01 18:43:14 -0600

    Cambios en el archivo

--- arreglos.php
+++ arreglos.php
@@ -1,16 +1,18 @@
 <?php
 
 $meses = array('Enero','Fefrero','Mazo','Abril','Mayo','Junio','Agosto','Septiembre','Octubre','noviembre','diciembre');
+$meses2 = array('Enero','Fefrero','Mazo','Abril','Mayo','Junio','Agosto','Septiembre','Octubre','noviembre','diciembre');
 
 $numeros = array(1,23,5,8,36,7,4,25,78,288);
+$numeros2 = array(1,23,5,8,36,7,4,25,78,288);
 
 // Ordena albabeticamente
 
 sort($meses);
 sort($numeros);
 // retroceder alfabrticamente
-rsort($meses);
-rsort($numeros);
+rsort($meses2);
+rsort($numeros2);
 
  ?>
 <!DOCTYPE html>
@@ -37,10 +39,10 @@ rsort($numeros);
 
          <br>
         <h1>Oirdenado en retroceso alfabeticamente</h1>
-         
+
 <?php
 
-         foreach($meses as $mes)
+         foreach($meses2 as $mes)
 	{
 
 		echo "<li>".$mes."</li>";
@@ -48,17 +50,26 @@ rsort($numeros);
 ?>
 <br>
 <h1>Numeros</h1>
-<?php 
-     
+<?php
+
 	 foreach($numeros as $num){
+	    echo "<li>".$num."</li>";
+	 }
+
+?>
+<br>
+<h1>Numeros</h1>
+<?php
+
+	 foreach($numeros2 as $num){
 	    echo "<li>".$num."</li>";
 	 }
 
 ?>
 
     </center>
-   
-    
-        
+
+
+
   </body>
 </html>
