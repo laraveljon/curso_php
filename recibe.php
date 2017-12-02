4a04038db1530bf83cf3a574313f98070232078c
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-02 11:20:46 -0600

    recibe los datos

---
+++ recibe.php
@@ -0,0 +1,61 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+
+switch ($nombre) {
+  case 'Enero':
+
+       echo "Feliz Nuevo AÑo";
+    break;
+    case 'Febrero':
+
+         echo "Dia del colchon y del amor ...";
+      break;
+      case 'Marzo':
+
+           echo "UN mes de mas ...";
+        break;
+        case 'Abril':
+
+             echo "Dia de del niño ...";
+          break;
+          case 'Mayo':
+
+               echo "Dia de de las madres ...";
+            break;
+            case 'Junio':
+
+                 echo "El jemelo de junio ...";
+              break;
+              case 'Julio':
+
+                   echo "El Verano llego ...";
+                break;
+                case 'Agosto':
+
+                     echo "Sigue las vacaciones ...";
+                  break;
+                  case 'Septiembre':
+
+                       echo "Dia de la independencia ...";
+                    break;
+                    case 'Octubre':
+
+                         echo "El otoño llego ...";
+                      break;
+                      case 'Novembre':
+
+                           echo "Dia de muertos ...";
+                        break;
+                        case 'Diciembre':
+
+                             echo "Feliz navidad ...";
+                          break;
+
+  default:
+    echo "Jonathan alanis Rojas";
+    break;
+}
+
+ ?>
