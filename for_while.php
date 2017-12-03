d4b365ef60d718bccca03048a69dbb4b23b2d4d5
Jonathan Alanis Rojas <jar_alanis@outlook.com>
2017-12-02 22:26:28 -0600

    Achivo para saber for and while

---
+++ for_while.php
@@ -0,0 +1,23 @@
+<?php
+
+
+     $meses = array('Enero','Febrero','Marzo','Abril',
+                     'Mayo','Junio','Julio','Agosto',
+                      'Septiembre','Octubre','Noviembre','Diciembre');
+
+
+                       for ($i=0; $i < count($meses); $i++) {
+                         echo $meses[$i]."<br>";
+                       }
+echo "<br><br>";
+$contador = 0;
+
+while ($contador <= count($meses)) {
+
+   echo $meses[$contador]."<br>";
+   $contador++;
+}
+
+
+
+ ?>
